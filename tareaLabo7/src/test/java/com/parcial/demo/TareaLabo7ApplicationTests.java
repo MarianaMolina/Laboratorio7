a58db91bff3e5b341aec0ea8f65ce96b28f2865a
Mariana Molina <37163483+MarianaMolina@users.noreply.github.com>
2020-06-16 08:55:29 -0600

    primer commit

---
+++ tareaLabo7/src/test/java/com/parcial/demo/TareaLabo7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parcial.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaLabo7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
